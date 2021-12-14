cb5af2f587e403354b818cb9016c761a15731655
Niculut Laurentiu <Laurentiu.Niculut2@hpecds.com>
2021-12-14 09:15:12 +0100

    added output examples

--- Output-code/outputvm.tf
+++ Output-code/outputvm.tf
@@ -1,4 +1,20 @@
 
+data "aws_ami" "ami1" {
+  #executable_users = ["self"]
+  most_recent = true
+  name_regex = "ubuntu*"
+  #owners = ["self"]
+  
+  owners = ["099720109477"] # Canonical
+}
+resource "aws_instance" "instance1" {
+  ami = data.aws_ami.ami1.id
+  instance_type = "t2.micro"
+    tags = {
+     Name = "firstvm"
+   }
+}
+
 data "aws_ami" "ami2" {
   #executable_users = ["self"]
   most_recent = true
